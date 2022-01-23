57daa22a85c6c0dc8d5515424e9518356a927e6c
v.korobov <v.korobov@corp.mail.ru>
2022-01-23 13:37:59 +0300

    Added auto version increasing on successful tests (2)

--- setup.py
+++ setup.py
@@ -1,9 +1,36 @@
 # Импорт недавно установленного пакета setuptools.
 import setuptools
 
-from version_increaser import auto_version_increase
 
-import async_icq
+import json
+import urllib.request
+
+
+def get_last_version():
+
+    request = urllib.request.urlopen(
+        "https://pypi.org/pypi/async-icq/json").read()
+
+    last_release = list(
+        json.loads(request).get('releases').keys())[-1]
+
+    return [int(i) for i in last_release.split('.')]
+
+
+def auto_version_increase():
+
+    major, middle, minor = get_last_version()
+
+    if minor < 10:
+        return f'{major}.{middle}.{minor + 1}'
+    else:
+        minor = 0
+        if middle < 10:
+            return f'{major}.{middle + 1}.{minor}'
+        else:
+            middle = 0
+            return f'{major + 1}.{middle}.{minor}'
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
