a6341ab0ff61f212b0f56b21b82afbafdf4fce32
v.korobov <v.korobov@corp.mail.ru>
2022-01-23 03:04:32 +0300

    Added tests to python-publish.yml

---
+++ tests/test_methods.py
@@ -0,0 +1,29 @@
+import os
+
+import pytest
+
+from async_icq.bot_class import AsyncBot
+
+
+prepare_bot = AsyncBot(
+        token=os.getenv('TOKEN'),
+        url=os.getenv('API_URL'),
+)
+
+ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID')
+
+
+@pytest.mark.asyncio
+async def test_send_text():
+
+    await prepare_bot.start_session()
+
+    response = await prepare_bot.send_text(
+        chatId=ADMIN_CHAT_ID,
+        text=f'Hi, @[{ADMIN_CHAT_ID}]'
+    )
+
+    resp_json = await response.json()
+
+    assert resp_json.get('msgId')
+    assert resp_json.get('ok')
