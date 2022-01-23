e742b5b385d9af4c43440c5f8857c3376e8a713f
v.korobov <v.korobov@corp.mail.ru>
2022-01-23 03:57:16 +0300

    Added test_methods.py::test_send_text_with_keyboard

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 from async_icq.bot import AsyncBot
+from async_icq.helpers import InlineKeyboardMarkup, KeyboardButton
 
 
 prepare_bot = AsyncBot(
@@ -19,6 +20,39 @@ async def test_send_text():
     response = await prepare_bot.send_text(
         chatId=ADMIN_CHAT_ID,
         text=f'Hi, @[{ADMIN_CHAT_ID}]'
+    )
+
+    resp_json = await response.json()
+
+    assert resp_json.get('msgId')
+    assert resp_json.get('ok')
+
+
+@pytest.mark.asyncio
+async def test_send_text_with_keyboard():
+
+    await prepare_bot.start_session()
+
+    markup = InlineKeyboardMarkup()
+
+    markup.row(
+        KeyboardButton(
+            text='Button URL',
+            url='https://mail.ru/'
+        )
+    )
+
+    markup.row(
+        KeyboardButton(
+            text='Button callback',
+            callbackData='button|callback'
+        )
+    )
+
+    response = await prepare_bot.send_text(
+        chatId=ADMIN_CHAT_ID,
+        text=f'Hi, @[{ADMIN_CHAT_ID}]',
+        inlineKeyboardMarkup=markup
     )
 
     resp_json = await response.json()
